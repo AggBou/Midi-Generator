5bb0db1141064ed4f01825b2c1b4aa36afce3afd
Aggelos <aggelospispiris@gmail.com>
2025-11-24 11:27:39 +0200

    Playback doesnt work, added a pyinstaller buiild spec file

--- main.py
+++ main.py
@@ -1,14 +1,68 @@
 # main.py
 import sys
+import ctypes
+import enum
 from PyQt5.QtWidgets import QApplication
 from gui import DrumGUI
+
+class SW(enum.IntEnum):
+
+    HIDE = 0
+    MAXIMIZE = 3
+    MINIMIZE = 6
+    RESTORE = 9
+    SHOW = 5
+    SHOWDEFAULT = 10
+    SHOWMAXIMIZED = 3
+    SHOWMINIMIZED = 2
+    SHOWMINNOACTIVE = 7
+    SHOWNA = 8
+    SHOWNOACTIVATE = 4
+    SHOWNORMAL = 1
+
+
+class ERROR(enum.IntEnum):
+
+    ZERO = 0
+    FILE_NOT_FOUND = 2
+    PATH_NOT_FOUND = 3
+    BAD_FORMAT = 11
+    ACCESS_DENIED = 5
+    ASSOC_INCOMPLETE = 27
+    DDE_BUSY = 30
+    DDE_FAIL = 29
+    DDE_TIMEOUT = 28
+    DLL_NOT_FOUND = 32
+    NO_ASSOC = 31
+    OOM = 8
+    SHARE = 26
+
+def bootstrap():
+    if ctypes.windll.shell32.IsUserAnAdmin():
+        main()
+    else:
+        hinstance = ctypes.windll.shell32.ShellExecuteW(
+            None, 'runas', sys.executable, sys.argv[0], None, SW.SHOWNORMAL
+        )
+        if hinstance <= 32:
+            raise RuntimeError(ERROR(hinstance))
 
 def main():
     app = QApplication(sys.argv)
     window = DrumGUI()
     window.resize(800, 400)
-    window.show()
+    window.show()    
+    if not ctypes.windll.shell32.IsUserAnAdmin():
+        print('Not enough priviledge, restarting...')
+        ctypes.windll.shell32.ShellExecuteW(
+            None, 'runas', sys.executable, ' '.join(sys.argv), None, None)
+    else:
+        print('Elevated privilege acquired')
     sys.exit(app.exec_())
 
+
+    
 if __name__ == "__main__":
     main()
+
+
--- midi_export.py
+++ midi_export.py
@@ -1,10 +1,5 @@
-import mido
-
-mid = mido.MidiFile()
-track = mido.MidiTrack()
-...
-track.append(mido.MetaMessage('set_tempo', tempo=tempo))
 from mido import MidiFile, MidiTrack, Message
+import mido
 
 def export_pattern(patterns, bpm, filename):
     mid = MidiFile()
